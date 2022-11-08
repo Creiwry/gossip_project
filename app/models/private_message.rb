bd19cf33e582842b2c1d4708df782ff82b4efe08
Creiwry <jay.gardos@outlook.com>
2022-11-08 14:10:23 +0100

    created IsNotSender validator, created seed file

--- app/models/private_message.rb
+++ app/models/private_message.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
+
+require_relative './validators/is_not_sender_validator'
+
+# PrivateMessage model
 class PrivateMessage < ApplicationRecord
   belongs_to :sender, class_name: 'User'
   belongs_to :recipient, class_name: 'User'
+  validates_with IsNotSender
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,57 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+require 'faker'
+
+City.destroy_all
+User.destroy_all
+Gossip.destroy_all
+Tag.destroy_all
+PrivateMessage.destroy_all
+
+10.times do
+  City.new(
+    name: Faker::Address.city,
+    zip_code: Faker::Address.zip
+  )
+end
+
+10.times do
+  User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    description: Faker::Lorem.paragraph,
+    email: Faker::Internet.email,
+    age: Faker::Number.between(from: 18, to: 100),
+    city: City.all.sample
+  )
+end
+
+20.times do
+  Gossip.new(
+    title: Faker::Lorem.sentence,
+    content: Faker::Lorem.paragraph,
+    user: User.all.sample
+  )
+end
+
+10.times do
+  Tag.new(
+    title: Faker::Lorem.word
+  )
+end
+
+Gossip.all.each do |gossip|
+  TagOfGossip.new(
+    gossip:,
+    tag: Tag.all.sample
+  )
+end
+
+10.times do
+  PrivateMessage.new(
+    content: Faker::Lorem.paragraph,
+    sender: User.all.sample,
+    recipient: User.all.sample
+  )
+end
