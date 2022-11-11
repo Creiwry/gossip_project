7605993ee6a86c0a5db6f6433916812d67dfb5c8
Creiwry <jay.gardos@outlook.com>
2022-11-11 17:54:19 +0100

    resources users and cities, controllers, and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get '/user/:id', to: 'user#show', as: 'user'
   get '/welcome/:name', to: 'welcome#show', as: 'welcome'
   get '/contacts', to: 'contacts#show'
   get '/team', to: 'team#show'
   resources :gossips, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show]
+  resources :cities, only: [:new, :create, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
