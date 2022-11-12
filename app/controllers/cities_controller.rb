9e2396cb7abc679349e1c5373e13f14c848cb341
Creiwry <jay.gardos@outlook.com>
2022-11-12 14:01:20 +0100

    show page for cities

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,24 +1,26 @@
+# frozen_string_literal: true
 
+# Cities Controller
 class CitiesController < ApplicationController
-  def index
-  end
+  def index; end
 
   def show
+    @city = City.find_by(id: params[:id])
+    @users = []
+    User.where(city_id: @city.id).each do |user|
+      puts user.id
+      @users << user.id
+    end
+    puts @users.class
   end
 
-  def new
-  end
+  def new; end
 
-  def create
+  def create; end
 
-  end
+  def edit; end
 
-  def edit
-  end
+  def update; end
 
-  def update
-  end
-
-  def destroy
-  end
+  def destroy; end
 end
