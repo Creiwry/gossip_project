5a7841f8c766de90e6322bd2e25ad9857da7ae4a
Creiwry <jay.gardos@outlook.com>
2022-11-16 15:10:15 +0100

    bcrypt, added password to user model and table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@
 
 # User model
 class User < ApplicationRecord
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
